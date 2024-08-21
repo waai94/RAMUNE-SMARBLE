5880a5cbb35deddc5a51638a813a2732c8e852fd
akodon <akodon2003@gmail.com>
2024-08-21 11:32:42 +0900

    タイトルのUISE、BGMをつけたにょ！シーン遷移もできるよ！

---
+++ Assets/Kanazawa/titleAudioScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class titleAudioandSceneScript : MonoBehaviour
+{
+    public AudioSource AudioSource;
+    public AudioClip UISE;      //UIのSE
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource = GetComponent<AudioSource>();
+    }
+
+    public void UIAudioplay()
+    {
+        //UIのSEを鳴らすよ！
+        AudioSource.PlayOneShot(UISE);
+    }
+
+    //ゲームシーンに切り替える
+    public void StartScene()
+    {
+        SceneManager.LoadScene("takmiscnee");
+    }
+}
